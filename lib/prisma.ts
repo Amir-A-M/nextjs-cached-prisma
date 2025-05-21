8115a9faa12c365800b24d93314b275bcb96e02e
Amir Abbas Mousavi <mamyrbas@gmail.com>
2025-05-21 16:38:35 +0330

    added cached-prisma and it just works!!

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,15 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
+// declare global {
+//   var prisma: PrismaClient | undefined
+// }
 
-const prisma = global.prisma || new PrismaClient()
+// const prisma = global.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
+// if (process.env.NODE_ENV === 'development') global.prisma = prisma
+
+import { Prisma } from "cached-prisma";
+
+const prisma = new Prisma().client;
 
 export default prisma
