f41d7a1bb8447cd20d19cb2552a6c89c0001ed44
Amir Abbas Mousavi <mamyrbas@gmail.com>
2025-05-22 13:26:32 +0330

    added prisma - added prisma - changed prisma output location - printing DB call time - refresh button - added createdAt to db schema

---
+++ src/components/refresh-button.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useTransition } from 'react'
+
+export default function RefreshButton() {
+  const router = useRouter()
+  const [isPending, startTransition] = useTransition()
+
+  return (
+    <button
+      className={`${
+        isPending ? 'cursor-not-allowed text-gray-400' : ''
+      } text-sm text-gray-700 hover:text-gray-950 border px-3 py-2 rounded-xl`}
+      disabled={isPending}
+      onClick={() => {
+        startTransition(() => {
+          router.refresh()
+        })
+      }}
+    >
+      {isPending ? 'Refreshing...' : 'Refresh'}
+    </button>
+  )
+}
